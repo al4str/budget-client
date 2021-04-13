27c631135d9e0ac8a73177fe6af5b29f684e488c
al4str <al4str.ftw@gmail.com>
2021-04-13 19:23:40 +0300

    finish docker run script

--- deploy/run.sh
+++ deploy/run.sh
@@ -6,4 +6,6 @@ docker build -t budget-client-image -f ./deploy/Dockerfile .
 docker run --name budget-client -d budget-client-image
 rm -rf ./public
 mkdir ./public
-docker cp budget-client:/dist ./public
+docker cp budget-client:/usr/src/app/dist/. ./public
+docker stop budget-client
+docker rm budget-client
